69b866be0f1768675006ba86fb9a23bbb1e51f1e
dhoniaridho <dhoniaridho@gmail.com>
2024-01-18 20:29:09 +0700

    feat: add fastify as base handler

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
 import { add } from "./functions/math";
+import Fastify from "fastify";
 
-console.log(add(11, 10000));
+const fastify = Fastify({
+    logger: true,
+});
+
+// Declare a route
+fastify.get("/", async function handler() {
+    console.log(add(11, 10000));
+    return { hello: "world" };
+});
+
+async function bootstrap() {
+    // Run the server!
+    try {
+        await fastify.listen({ port: 3333 });
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+}
+
+bootstrap();
