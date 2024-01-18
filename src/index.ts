a251531c1209287149016de66dc1af6d7a8d81af
dhoniaridho <dhoniaridho@gmail.com>
2024-01-18 20:29:33 +0700

    feat: add fastify as base handler

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ fastify.get("/", async function handler() {
 async function bootstrap() {
     // Run the server!
     try {
-        await fastify.listen({ port: 3333 });
+        await fastify.listen({ port: 3000 });
     } catch (err) {
         fastify.log.error(err);
         process.exit(1);
